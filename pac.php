99e11558505944186d36b12bd5e147abe13d61a2
andreivims <andreivims@gmail.com>
2021-01-21 00:54:11 -0400

    20-01-21

--- pac.php
+++ pac.php
@@ -129,7 +129,7 @@
 					<table class='tableCabecalho'>
 						<tr>
 							<td rowspan='2' class='cabecalho1'>
-								<div class='logo b_all brz' ><img src='granjinha.jpeg' alt='Granjinha' width=90 height=90></div>
+								<div class='logo ' ><img src='granjinha.jpeg' alt='Granjinha' width=90 height=90></div>
 								".($campo1)."
 							</td> <td class='cabecalho2'>".($campo2)."</td> 
 						</tr>
@@ -166,7 +166,7 @@
 						position: relative;
 						float: left;
 						
-						margin-top: 10px;
+						margin-top: 7px;
 						
 						border: 1px solid black;
 						border-collapse: collapse;
@@ -175,6 +175,12 @@
 						width: ".($PAC->width)."px;
 						
 						background-color: rgb(245,245,245);
+					}
+					
+					.infoTD{
+						
+						padding: 7px;
+				
 					}
 					
 					
@@ -189,7 +195,7 @@
 				
 				for($i=0; $i<count($arrayInfos); $i++){
 					
-					echo"<td>".($arrayInfos[$i])."</td> ";
+					echo"<td class='infoTD'>".($arrayInfos[$i])."</td> ";
 				}
 				
 							
@@ -201,11 +207,157 @@
 		}
 		
 		
+		class ContentPAC3{
+			
+			
+			
+			
+			public function cssContentPAC3($PAC){
+				
+				 echo "
+			 
+					.contentpac3dias{
+						
+						position: relative;
+						float: left;
+						
+						margin-top: 7px;
+						
+						width: ".($PAC->width)."px;
+						
+						text-align: center;
+						border: 1px solid black;
+						border-collapse: collapse;
+					}
+					
+					
+				 
+				";
+		
+			}
+			
+			public function htmlContentPAC3($mes, $ano){
+				
+				 $NDias = cal_days_in_month( CAL_GREGORIAN , $mes , $ano );
+		
+				 $diaadia = array();
+				 $i=0; $j=0;
+				 while( $i < $NDias )
+				{
+					 
+					 $data = date($ano . '-'. $mes .'-' . ($i+1));
+					 $diasemana_numero = date('w', strtotime($data));
+					 //echo "<br>". ($i+1)%7 ." === ". $diasemana_numero;
+					 
+					 if($i == 0)
+					{
+						 if(($i+1)<10){ $diaadia[$j]="0". ($i+1) ." ao "; }
+						 else { $diaadia[$j]=($i+1) ." ao ";}
+						 
+						 
+						 if($diasemana_numero == 6)
+						{
+							
+							 if(($i+1)<10){ $diaadia[$j++] .="0". ($i+1); }
+							 else { $diaadia[$j++] .=($i+1) ;}
+							 
+							 if(($i+2)<10){ $diaadia[$j]="0". ($i+2) ." ao "; }
+							 else { $diaadia[$j]=($i+2) ." ao ";}
+						}
+						 
+					}else if($diasemana_numero == 6)
+					{
+						 
+						 if(($i+1)<10){ $diaadia[$j++] .="0". ($i+1); }
+						 else { $diaadia[$j++] .=($i+1);}
+						 
+						 if(($i+2)<10){ $diaadia[$j]= "0". ($i+2) ." ao "; }
+						 else { $diaadia[$j]=($i+2) ." ao ";}
+						 
+					}else if($i == $NDias-1)
+					{
+						 
+						 if(($i+1)<10){ $diaadia[$j++] .="0". ($i+1); }
+						 else { $diaadia[$j++] .=($i+1);}
+					}
+					 
+					 
+					 $i++;
+				}
+				
+/* ====================================================================================== */		
+				
+				echo "<table class='contentpac3dias'>
+			
+					<tr> 
+						<td class='bgGrey'><b>Semana dos dias:</b></td>";
+						
+						for($i=0; $i<count($diaadia); $i++){
+								echo "<td  class='bgGrey' colspan='3'><b>". ($diaadia[$i]) ." </b></td>";	
+						}
+							
+				echo "	
+					</tr>
+					
+					<tr> 
+						<td  class='bgGrey'><b>Área de Inspeção</b></td> ";
+						
+					for($i=0; $i<count($diaadia); $i++){
+								echo "<td  class='bgGrey'><b>Lux</b></td>  <td  class='bgGrey'><b>&nbsp C &nbsp </b></td>  <td  class='bgGrey'><b>NC&nbsp </b></td>";	
+					}
+				
+				echo "
+					</tr>	
+					<tr> <td  class='bgGrey'>Recepção de Ovos</td> "; 
+						for($i=0; $i<count($diaadia); $i++){
+								echo "<td>&nbsp </td>  <td>&nbsp </td>  <td>&nbsp </td>";	
+						}
+				echo "		
+					</tr>
+						
+					<tr> <td  class='bgGrey'>Classificação</td>";
+				 	
+						for($i=0; $i<count($diaadia); $i++){
+								echo "<td>&nbsp </td>  <td>&nbsp </td>  <td>&nbsp </td>";	
+						}
+				echo "	
+					</tr>
+						
+					<tr> <td  class='bgGrey'>Depósito/Expedição</td> ";
+						for($i=0; $i<count($diaadia); $i++){
+								echo "<td>&nbsp </td>  <td>&nbsp </td>  <td>&nbsp </td>";	
+						}
+				echo "
+					</tr>
+						
+					<tr> <td  class='bgGrey'>Sanitário/Vestiário</td>"; 
+						for($i=0; $i<count($diaadia); $i++){
+								echo "<td>&nbsp </td>  <td>&nbsp </td>  <td>&nbsp </td>";	
+						}
+				echo "
+					</tr>
+						
+					<tr> <td  class='bgGrey'>Depósito Embalagens</td>"; 
+						for($i=0; $i<count($diaadia); $i++){
+								echo "<td>&nbsp </td>  <td>&nbsp </td>  <td>&nbsp </td>";	
+						}
+				echo " 
+					</tr>
+						
+				
+				</table>";	
+			}
+			
+			
+			
+		}
+		
+		
 		if($CONTROLE == "pac1"){
 			
 		}else{
 			
-			$heightGeral = 400;
+			$heightGeral = 600;
 			$widthGeral = 1050;
 			$pac = new PacGeral($heightGeral, $widthGeral);
 			
@@ -217,6 +369,13 @@
 			
 			$infoHeight = 80;
 			$info = new Informativo($infoHeight);
+			
+			
+			
+			$pac3 = new ContentPAC3();
+			
+			
+			
 		}
 		
 
@@ -250,14 +409,55 @@
 	
 				$cabecalho->cssCabecalho($pac);
 				$info->cssInformativo($pac);
+				
+				
+				
+				$pac3->cssContentPAC3($pac);
 		 
 ?>
+
+				
+				
+				
+				
+					 .legenda
+					{
+						
+						 width: <?PHP echo".($pac->width)."; ?>;
+						 text-align: center;
+						 padding: 5px;
+						 
+						  
+					}
+					
+					
+					.monitor
+					{
+						 //height: 100px;
+						 width: <?PHP echo".($pac->width)."; ?>;
+						 text-align: center;
+						 margin-top: 20px;
+					}
+					
+				.bgGrey{
+					 background-color: rgb(245,245,245);
+				}
+				
+				.b_all
+				{
+					 border-collapse: collapse; 
+					 border: 1px solid black;
+				}
+				
+				tr,td{
+					 padding: 3px;
+				}
 			
 			</style>
 			
 		</head>
 		
-		<body>
+		<body style='font-family: ARIAL;'>
 			
 			<div style="height: <?PHP echo $pac->height; ?>px; width: <?PHP echo $pac->width; ?>px; border: 1px solid black;">
 <?PHP
@@ -267,12 +467,34 @@
 				
 				$cabecalho->htmlCabecalho("CHECK-LIST DE LUMINÁRIAS", "PAC 03 - FML 11", $mesEscrito ." - ". $ano);
 				
-				$infos = array("1111111", "2222222", "33333333 lksdajflk kjlskdfjlka  ksdjkalfsj jklsjladkfjksd   jsdaklfljksdfk   sjdklafjkl  jlksadjlf<Br> lsakdfjk  sadlkfjl kjsadf l", "444444");
+				$infos = array(
+					"<b>Período:</b> <br>Semanal", 
+					"<b>Parâmetros Avaliados:</b> <br>Lâmpada: queimada ou solta. Proteção quebrada ou suja.<br> Medir Lux: Usar luxímetro", 
+					"<b>Ações Corretivas:</b> <br>Trocar lâmpada; Fixar/trocar a lâmpada;  Trocar a proteção; Realizar limpeza e secar; Colocar lâmpadas mais potentes."
+				);
+				
 				$info->htmlInformativo($infos);
+				
+				
+				
+				$pac3->htmlContentPAC3($mes, $ano);
+				
 				
 			}
 		 
+		 
+		 
+		 
+		 
+		 
 ?>			
+			
+			
+			 
+				 <div class="legenda b_all ">  <b>LEGENDA:</b>(C) Conforme; (NC) Não conforme; </div>
+				 <div class="monitor  "> <b> Responsável pelo Monitoramento: _______________________________________________ </b> </div>
+			
+
 			
 			</div>
 			
