cfc37d25c3cfb5e85c24fc95d8d12f108d22fe5c
andreivims <andreivims@gmail.com>
2021-05-06 00:43:19 -0400

    05-05-2021

--- index.php
+++ index.php
@@ -7,8 +7,365 @@
 	 $testecampos->setConteudoHTML("input", "", "teste");
 	 //$teste = new GenericaTabelaBD();
 	
+/////////////////////////////////////////////////////////////////	
+/////////////////////////////////////////////////////////////////	
+/////////////////////////////////////////////////////////////////	
+/////////////////////////////////////////////////////////////////	
+/////////////////////////////////////////////////////////////////	
+/////////////////////////////////////////////////////////////////	
+/////////////////////////////////////////////////////////////////	
+/////////////////////////////////////////////////////////////////	
+	
+	
+	
+		
+	 $loteProducao = array();
+	 $loteProducao[0] = 0.0;
+	 $loteProducao[1] = 0.0;
+	 $loteProducao[2] = 0.0;
+	 $loteProducao[3] = 0.0;
+	 $loteProducao[4] = 0.0;
+	 $loteProducao[5] = 0.0;
+	 $loteProducao[6] = 0.0;
+	 $loteProducao[7] = 0.0;
+	 $loteProducao[8] = 0.0;
+	 $loteProducao[9] = 0.0;
+	 $loteProducao[10] = 0.0;
+	 $loteProducao[11] = 0.0;
+	 $loteProducao[12] = 0.0;
+	 $loteProducao[13] = 0.0;
+	 $loteProducao[14] = 0.0;
+	 $loteProducao[15] = 0.0;
+	 $loteProducao[16] = 0.0;
+	 $loteProducao[17] = 0.1;
+	 $loteProducao[18] = 0.3;
+	 $loteProducao[19] = 0.6;
+	 $loteProducao[20] = 0.8;
+	 $loteProducao[21] = 0.9;
+	 $loteProducao[22] = 0.91;
+	 $loteProducao[23] = 0.92;
+	 $loteProducao[24] = 0.93;
+	 $loteProducao[25] = 0.94;
+	 $loteProducao[26] = 0.95;
+	 $loteProducao[27] = 0.95;
+	 $loteProducao[28] = 0.95;
+	 $loteProducao[29] = 0.95;
+	 $loteProducao[30] = 0.95;
+	 $loteProducao[31] = 0.94;
+	 $loteProducao[32] = 0.94;
+	 $loteProducao[33] = 0.94;
+	 $loteProducao[34] = 0.94;
+	 $loteProducao[35] = 0.93;
+	 $loteProducao[36] = 0.93;
+	 $loteProducao[37] = 0.93;
+	 $loteProducao[38] = 0.93;
+	 $loteProducao[39] = 0.92;
+	 $loteProducao[40] = 0.92;
+	 $loteProducao[41] = 0.92;
+	 $loteProducao[42] = 0.92;
+	 $loteProducao[43] = 0.91;
+	 $loteProducao[44] = 0.91;
+	 $loteProducao[45] = 0.91;
+	 $loteProducao[46] = 0.91;
+	 $loteProducao[47] = 0.9;
+	 $loteProducao[48] = 0.9;
+	 $loteProducao[49] = 0.89;
+	 $loteProducao[50] = 0.89;
+	 $loteProducao[51] = 0.89;
+	 $loteProducao[52] = 0.88;
+	 $loteProducao[53] = 0.88;
+	 $loteProducao[54] = 0.87;
+	 $loteProducao[55] = 0.87;
+	 $loteProducao[56] = 0.87;
+	 $loteProducao[57] = 0.86;
+	 $loteProducao[58] = 0.86;
+	 $loteProducao[59] = 0.85;
+	 $loteProducao[60] = 0.85;
+	 $loteProducao[61] = 0.85;
+	 $loteProducao[62] = 0.84;
+	 $loteProducao[63] = 0.84;
+	 $loteProducao[64] = 0.83;
+	 $loteProducao[65] = 0.83;
+	 $loteProducao[66] = 0.83;
+	 $loteProducao[67] = 0.82;
+	 $loteProducao[68] = 0.82;
+	 $loteProducao[69] = 0.81;
+	 $loteProducao[70] = 0.81;
+	 $loteProducao[71] = 0.8;
+	 $loteProducao[72] = 0.8;
+	 $loteProducao[73] = 0.79;
+	 $loteProducao[74] = 0.78;
+	 $loteProducao[75] = 0.77;
+	 $loteProducao[76] = 0.76;
+	 $loteProducao[77] = 0.75;
+	 $loteProducao[78] = 0.74;
+	 $loteProducao[79] = 0.73;
+	 $loteProducao[80] = 0.72;
+	 $loteProducao[81] = 0.72;
+	 $loteProducao[82] = 0.72;
+	 $loteProducao[83] = 0.72;
+	 $loteProducao[84] = 0.72;
+	 $loteProducao[85] = 0.71;
+	 $loteProducao[86] = 0.71;
+	 $loteProducao[87] = 0.71;
+	 $loteProducao[88] = 0.71;
+	 $loteProducao[89] = 0.7;
+	 
+	 $loteConsumo = array();
+	 $loteConsumo[0] = 14;
+	 $loteConsumo[1] = 19;
+	 $loteConsumo[2] = 24;
+	 $loteConsumo[3] = 28;
+	 $loteConsumo[4] = 35;
+	 $loteConsumo[5] = 39;
+	 $loteConsumo[6] = 42;
+	 $loteConsumo[7] = 46;
+	 $loteConsumo[8] = 50;
+	 $loteConsumo[9] = 54;
+	 $loteConsumo[10] = 60;
+	 $loteConsumo[11] = 64;
+	 $loteConsumo[12] = 70;
+	 $loteConsumo[13] = 73;
+	 $loteConsumo[14] = 76;
+	 $loteConsumo[15] = 79;
+	 $loteConsumo[16] = 81;
+	 $loteConsumo[17] = 85;
+	 $loteConsumo[18] = 89;
+	 $loteConsumo[19] = 93;
+	 $loteConsumo[20] = 98;
+	 $loteConsumo[21] = 102;
+	 $loteConsumo[22] = 105;
+	 $loteConsumo[23] = 108;
+	 $loteConsumo[24] = 110;
+	 $loteConsumo[25] = 111;
+	 $loteConsumo[26] = 111;
+	 $loteConsumo[27] = 111;
+	 $loteConsumo[28] = 111;
+	 $loteConsumo[29] = 111;
+	 $loteConsumo[30] = 112;
+	 $loteConsumo[31] = 112;
+	 $loteConsumo[32] = 112;
+	 $loteConsumo[33] = 112;
+	 $loteConsumo[34] = 112;
+	 $loteConsumo[35] = 112;
+	 $loteConsumo[36] = 112;
+	 $loteConsumo[37] = 112;
+	 $loteConsumo[38] = 112;
+	 $loteConsumo[39] = 112;
+	 $loteConsumo[40] = 112;
+	 $loteConsumo[41] = 112;
+	 $loteConsumo[42] = 112;
+	 $loteConsumo[43] = 112;
+	 $loteConsumo[44] = 111;
+	 $loteConsumo[45] = 111;
+	 $loteConsumo[46] = 111;
+	 $loteConsumo[47] = 111;
+	 $loteConsumo[48] = 111;
+	 $loteConsumo[49] = 111;
+	 $loteConsumo[50] = 111;
+	 $loteConsumo[51] = 111;
+	 $loteConsumo[52] = 110;
+	 $loteConsumo[53] = 110;
+	 $loteConsumo[54] = 110;
+	 $loteConsumo[55] = 110;
+	 $loteConsumo[56] = 110;
+	 $loteConsumo[57] = 110;
+	 $loteConsumo[58] = 110;
+	 $loteConsumo[59] = 110;
+	 $loteConsumo[60] = 110;
+	 $loteConsumo[61] = 110;
+	 $loteConsumo[62] = 110;
+	 $loteConsumo[63] = 110;
+	 $loteConsumo[64] = 110;
+	 $loteConsumo[65] = 110;
+	 $loteConsumo[66] = 110;
+	 $loteConsumo[67] = 110;
+	 $loteConsumo[68] = 110;
+	 $loteConsumo[69] = 110;
+	 $loteConsumo[70] = 110;
+	 $loteConsumo[71] = 110;
+	 $loteConsumo[72] = 110;
+	 $loteConsumo[73] = 110;
+	 $loteConsumo[74] = 110;
+	 $loteConsumo[75] = 110;
+	 $loteConsumo[76] = 110;
+	 $loteConsumo[77] = 110;
+	 $loteConsumo[78] = 110;
+	 $loteConsumo[79] = 110;
+	 $loteConsumo[80] = 110;
+	 $loteConsumo[81] = 110;
+	 $loteConsumo[82] = 110;
+	 $loteConsumo[83] = 110;
+	 $loteConsumo[84] = 110;
+	 $loteConsumo[85] = 110;
+	 $loteConsumo[86] = 110;
+	 $loteConsumo[87] = 110;
+	 $loteConsumo[88] = 110;
+	 $loteConsumo[89] = 110;
+	 
+	 
+	 
+	class LoteGalinhas{
+		 public $mesAloja;
+		 public $semanaDescarte;
+		 public $semanaAlojado;
+		 public $producaoTotal;
+		 public $consumoTotal;
+		 
+		 
+		public function __construct($ma, $sd){
+			 $this->mesAloja = $ma;
+			 $this->semanaDescarte = $sd;
+			 $this->semanaAlojado = 0;
+			 $this->producaoTotal = 0;
+			 $this->consumoTotal = 0;
+		}
+		
+		public function Alojamento($dia, $loteProducao, $loteConsumo){
+			 
+			if(($this->semanaAlojado>0 && $this->semanaAlojado<$this->semanaDescarte)|| ($this->semanaAlojado == 0 && $this->mesAloja == date('m', $dia))){
+			 
+			 
+				 $ProducaoCXs = ($loteProducao[$this->semanaAlojado] * 7 * 2000)/360;
+				 echo" - ProducaoCXs=". number_format($ProducaoCXs, 2, ',', '.');
+				 $this->producaoTotal += $ProducaoCXs;
+				 
+				 
+				 $consumoKg = ($loteConsumo[$this->semanaAlojado] * 7 * 2000)/1000;
+				 echo" - ConsumoKg=". $consumoKg;
+				 $this->consumoTotal += $consumoKg;
+				 
+				if($ProducaoCXs>0){
+					 
+					 $KG_Caixa = ($consumoKg / $ProducaoCXs);
+					 echo" - KG_Caixa=".  number_format($KG_Caixa, 2, ',', '.');
+				 
+				}
+
+
+				
+				 
+				 $this->semanaAlojado++;
+				 echo"--- Alojado=". ($this->semanaAlojado);
+				 
+				 
+			}
+
+		}
+		
+		
+		 
+	}
+	 
+	 
+	 $lote1 = new LoteGalinhas(1,90);
+	 
+	 
+	 
+	 $lote1AlojaMes = 1;
+	 $lote1DescarteSemana = 90;
+	 $lote1Alojado = 0;
+	 $lote1TotalProducao = 0;
+	 $lote1TotalConsumo = 0;
+	 
+	for($i=0; $i<140; $i++){
+		
+		 $dia = strtotime('+'. $i .' weeks', strtotime(date("d/m/Y")));
+		 
+		 
+		 
+		 echo "<br> ". date('d/m/Y', $dia) ." - ". date('m', $dia);	
+		 
+		 $lote1->Alojamento($dia, $loteProducao, $loteConsumo);
+
+		 echo" - ProducaoCXs=".  number_format($lote1->producaoTotal, 2, ',', '.'); 
+		 echo" - ConsumoKg=". number_format($lote1->consumoTotal, 2, ',', '.');
+		 echo" - KG_Caixa=".  number_format($lote1->consumoTotal / $lote1->producaoTotal, 2, ',', '.');
+				 
+		
+	}
+		 
+	 echo"<br><br>RESULTADO LOTE1";
+	 echo"<br>lote1TotalProducaoCXs = ".  number_format($lote1->producaoTotal, 2, ',', '.'); 
+	 echo"<br>lote1TotalConsumoKG = ". number_format($lote1->consumoTotal, 2, ',', '.');
+	 echo"<br>Media KG Por Caixa = ".  number_format($lote1->consumoTotal / $lote1->producaoTotal, 2, ',', '.');
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 /*
+	 $lote1AlojaMes = 1;
+	 $lote1DescarteSemana = 90;
+	 $lote1Alojado = 0;
+	 $lote1TotalProducao = 0;
+	 $lote1TotalConsumo = 0;
+	 
+	for($i=0; $i<140; $i++){
+		
+		 $dia = strtotime('+'. $i .' weeks', strtotime(date("d-m-Y")));
+		 
+		 echo "<br> ". date('d/m/Y', $dia) ." - ". date('m', $dia);	
+
+		if(($lote1Alojado>0 && $lote1Alojado<$lote1DescarteSemana)|| ($lote1Alojado == 0 && $lote1AlojaMes == date('m', $dia))){
+			 
+			 
+			 
+			 
+			 $ProducaoCXs = ($loteProducao[$lote1Alojado] * 7 * 2000)/360;
+			 echo" - ProducaoCXs=". number_format($ProducaoCXs, 2, ',', '.');
+			 $lote1TotalProducao += $ProducaoCXs;
+			 
+			 
+			 $consumoKg = ($loteConsumo[$lote1Alojado] * 7 * 2000)/1000;
+			 echo" - ConsumoKg=". $consumoKg;
+			 $lote1TotalConsumo += $consumoKg;
+			 
+			if($ProducaoCXs>0){
+				 
+				 $KG_Caixa = ($consumoKg / $ProducaoCXs);
+				 echo" - KG_Caixa=".  number_format($KG_Caixa, 2, ',', '.');
+			 
+			}
+
+
+			
+			 
+			 $lote1Alojado++;
+			 echo"--- Alojado=". ($lote1Alojado);
+			 
+			 
+		}
+
+			 
+		
+	}
+		 
+	 echo"<br><br>RESULTADO LOTE1";
+	 echo"<br>lote1TotalProducaoCXs = ".  number_format($lote1TotalProducao, 2, ',', '.'); 
+	 echo" <br>lote1TotalConsumoKG = ". number_format($lote1TotalConsumo, 2, ',', '.');
+	 echo"<br>Media KG Por Caixa = ".  number_format($lote1TotalConsumo / $lote1TotalProducao, 2, ',', '.');
+	 
+	 
+	 */
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	
+	
+	
+	
+	
 
 
 ?>
-
-lajfasdj
